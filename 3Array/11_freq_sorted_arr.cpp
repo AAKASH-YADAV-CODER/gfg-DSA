c54be53ecfa2156a8b9795e045b1db8f2fe71fc9
Aakash Yadav <ay614838@gmail.com>
2025-04-06 15:22:18 +0530

    finding frequency in sorted array naive sol done efficient way

--- 3Array/11_freq_sorted_arr.cpp
+++ 3Array/11_freq_sorted_arr.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include <iostream>
-#include <vector>
 using namespace std;
 // Naive Method 1>
 // void printFreq(vector<int> &arr, int N) {
@@ -23,29 +22,58 @@ using namespace std;
 //   }
 // }
 // Or By this way.
-void printFreq(vector<int> &arr, int n) {
+// void printFreq(vector<int> &arr, int n) {
+//   int freq = 1;
+//   int idx = 1;
+//   int value = arr[0];
+//   while (idx < n) {
+//
+//     // check if the current value is equal to
+//     // previous value.
+//     if (arr[idx - 1] == arr[idx]) {
+//       freq++;
+//       idx++;
+//     } else {
+//       cout << value << " " << freq << endl;
+//       value = arr[idx];
+//       idx++;
+//
+//       // reset the frequency
+//       freq = 1;
+//     }
+//   }
+//
+//   // print the last value and its frequency
+//   cout << value << " " << freq;
+// }
+
+// Efficient Method
+void printFreq(vector<int> &arr, int N) {
+
+  // Stores the frequency of an element
   int freq = 1;
-  int idx = 1;
-  int value = arr[0];
-  while (idx < n) {
 
-    // check if the current value is equal to
-    // previous value.
-    if (arr[idx - 1] == arr[idx]) {
+  // Traverse the array arr[]
+  for (int i = 1; i < N; i++) {
+
+    // If the current element is equal
+    // to the previous element
+    if (arr[i] == arr[i - 1]) {
+
+      // Increment the freq by 1
       freq++;
-      idx++;
-    } else {
-      cout << value << " " << freq << endl;
-      value = arr[idx];
-      idx++;
+    }
 
-      // reset the frequency
+    // Otherwise,
+    else {
+      cout << "Frequency of " << arr[i - 1] << " is: " << freq << endl;
+      // Update freq
       freq = 1;
     }
   }
 
-  // print the last value and its frequency
-  cout << value << " " << freq;
+  // Print the frequency of the last element
+  cout << "Frequency of " << arr[N - 1] << " is: " << freq << endl;
 }
 int main() {
   vector<int> arr = {1, 1, 1, 2, 3, 3, 5, 5, 8, 8, 8, 9, 9, 10};
@@ -68,5 +96,17 @@ Time Complexity: O(N)
 Auxiliary Space: O(N)
 
 
+Efficient Approach: The above approach can be optimized in terms of space used
+based on the fact that, in a sorted array, the same elements occur
+consecutively, so the idea is to maintain a variable to keep track of the
+frequency of elements while traversing the array. Follow the steps below to
+solve the problem:
+
+Initialize a variable, say freq as 1 to store the frequency of elements.
+Iterate in the range [1, N-1] using the variable i and perform the following
+steps: If the value of arr[i] is equal to arr[i-1], increment freq by 1. Else
+print value the frequency of arr[i-1] obtained in freq and then update freq
+to 1. Finally, after the above step, print the frequency of the last distinct
+element of the array as freq.
 
  */
