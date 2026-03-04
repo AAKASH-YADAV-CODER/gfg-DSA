7772187c48ca61be8ce96b39c9cf4a8d25f51476
Aakash Yadav <ay614838@gmail.com>
2026-03-04 18:53:43 +0530

    Problem find maximum consecutive ones and bool array with having 0's and 1's in array

--- 3Array/28_consecutive_ones.cpp
+++ 3Array/28_consecutive_ones.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
+
+// it's navie solution with O(n2) T.C and O(1) A.S
 int maxConsecutive(bool arr[], int n) {
   int res = 0;
   for (int i = 0; i < n; i++) {
@@ -16,10 +18,26 @@ int maxConsecutive(bool arr[], int n) {
   }
   return res;
 }
+
+// Efficient Solution in O(n) T.C and O(1) A.S
+int maxConsecutiveEfficient(bool arr[], int n) {
+  int res = 0;
+  int curr = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == 0) {
+      curr = 0;
+    } else {
+      curr++;
+      res = max(res, curr);
+    }
+  }
+  return res;
+}
 int main() {
   bool arr[] = {1, 0, 1, 1, 1, 1, 0, 0};
   int size = sizeof(arr) / sizeof(arr[0]);
-  int maxCons = maxConsecutive(arr, size);
+  // int maxCons = maxConsecutive(arr, size);
+  int maxCons = maxConsecutiveEfficient(arr, size);
   cout << "Hi Aakash lets start with neovim" << endl << maxCons << endl;
   return 0;
 }
